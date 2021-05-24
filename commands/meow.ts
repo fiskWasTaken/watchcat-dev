18d7b899fbcbbc5381243c3d82d1ca280b983e5a
fisk <fiskie@live.co.uk>
2021-05-24 23:12:06 +0100

    meow.ts

---
+++ commands/meow.ts
@@ -0,0 +1,19 @@
+import {Guild} from "discord.js";
+import {Env} from "../index";
+
+function choose(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+module.exports = (env: Env) => {
+    return {
+        description: ":3",
+        callable: async (guild: Guild, args: any[]) => {
+            const words = ["nyaaa", "meow", "nyan"];
+            const emoji = ["=w=", "^w^", ":3", ":33", ":333", ";3", "=w=", "@w@", "@w@;"];
+
+            return `${choose(words)} ${choose(emoji)}`;
+        },
+        name: "meow"
+    }
+};
